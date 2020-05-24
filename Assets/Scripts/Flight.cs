2a35266745c8e7a22a685be09c9991fc9f6bfa51
Christopher <32484465+ChristopherBabb@users.noreply.github.com>
2020-05-24 17:42:02 +0200

    Reformatted and serialized JSON

---
+++ Assets/Scripts/Flight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Flight
+{
+    public string id;
+    public string company;
+    public int duration;
+    public string destination;
+    public float latitude;
+    public float longitude;
+    public string departureTime;
+    public string arrivalTime;
+    public int price;
+    public string stopdestination;
+
+}
---
+++ Assets/Scripts/FlightLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlightLoader : MonoBehaviour
+{
+    public List<Flight> flights;
+
+    void Awake()
+    {
+        flights = new List<Flight>();
+        FlightDictionary dictionary = JsonUtility.FromJson<FlightDictionary>(JsonFileReader.LoadJsonAsResource("FlightDictionary.json"));
+        foreach(string dictionaryItem in dictionary.flights) 
+        {
+            LoadFlight(dictionaryItem);
+        }
+    
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadFlight(string path){
+        string myLoadedItem = JsonFileReader.LoadJsonAsResource(path);
+        Debug.Log("my flights " + myLoadedItem);
+        Flight myFlights = JsonUtility.FromJson<Flight>(myLoadedItem);
+        flights.Add(myFlights);
+    }
+}
